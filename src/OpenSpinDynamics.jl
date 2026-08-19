757bbfd91616439780ce0b6eed1a6b264467edb3
javahedi <javahedi@gmail.com>
2026-08-19 21:23:59 +0200

    refactor: define explicit public API

--- src/OpenSpinDynamics.jl
+++ src/OpenSpinDynamics.jl
@@ -1,40 +1,80 @@
-#__precompile__(false)
 module OpenSpinDynamics
 
-    using Reexport
+include("PauliOps.jl")
+using .PauliOps: generate_operators
 
-    include("PauliOps.jl")
-    @reexport using .PauliOps
+include("Coupling.jl")
+using .Coupling:
+    AbstractCoupling,
+    LongRangeCouplingDisorder,
+    LongRangeCouplingClean,
+    NearestNeighborCoupling,
+    get_matrix,
+    get_N
 
-    include("Coupling.jl")
-    @reexport using .Coupling
+include("QuantumState.jl")
+using .QuantumState:
+    AbstractInitialState,
+    NeelState,
+    PolarizedState,
+    construct_state
 
-    include("QuantumState.jl")
-    @reexport using .QuantumState
+include("SpinModels.jl")
+using .SpinModels:
+    SpinModel,
+    model,
+    update_model!
 
-    include("SpinModels.jl")
-    @reexport using .SpinModels
+include("LindbladSolver.jl")
+using .LindbladSolver:
+    LindbladSystem,
+    evolve_Lindblad
 
-    include("LindbladSolver.jl")
-    @reexport using .LindbladSolver
+include("KrylovArnoldiSolver.jl")
+using .KrylovArnoldiSolver:
+    KrylovArnoldiSystem,
+    evolve_KrylovArnodli
 
-    include("KrylovArnoldiSolver.jl")
-    @reexport using .KrylovArnoldiSolver
+include("StochasticWavefunctionSolver.jl")
+using .StochasticWavefunctionSolver:
+    StochasticWavefunctionSystem,
+    evolve_swf
 
-    include("StochasticWavefunctionSolver.jl")
-    @reexport using .StochasticWavefunctionSolver
+include("Solvers.jl")
+using .Solvers: solver_function
 
-    # Add new modules
-    include("Solvers.jl")
-    @reexport using .Solvers
+include("Disorder.jl")
+using .Disorder: disorder
 
-    include("Disorder.jl")
-    @reexport using .Disorder
+include("setup.jl")
+include("utils.jl")
 
-    include("setup.jl")
-    export setup_parameters, initialize_system
-
-    include("utils.jl")
-    export setup_logging, summarize_results
+export
+    generate_operators,
+    AbstractCoupling,
+    LongRangeCouplingDisorder,
+    LongRangeCouplingClean,
+    NearestNeighborCoupling,
+    get_matrix,
+    get_N,
+    AbstractInitialState,
+    NeelState,
+    PolarizedState,
+    construct_state,
+    SpinModel,
+    model,
+    update_model!,
+    LindbladSystem,
+    KrylovArnoldiSystem,
+    StochasticWavefunctionSystem,
+    evolve_Lindblad,
+    evolve_KrylovArnodli,
+    evolve_swf,
+    solver_function,
+    disorder,
+    setup_parameters,
+    initialize_system,
+    setup_logging,
+    summarize_results
 
 end
